35b1f88d434e3ba3f5050c6b8e801ebb06c8716f
John Pickett <pickett.bill@gmail.com>
2024-11-21 20:34:21 -0330

    initial shell layout, columns and rows and print_at

--- Monolith.py
+++ Monolith.py
@@ -8,27 +8,25 @@ def file_resource(*names):
     # return an os-independent filename inside data path
     return os.path.join('data', *names)
 
-def centre(bigger, smaller):
-    # helper function that returns a centred position
-    return int((bigger / 2) - (smaller / 2))
-
 class Monolith:
     def __init__(self):
         pygame.init()
+        screen_x, screen_y = 1920, 1080
+        self.border_size = 16
+        self.font_size = 16
         self.mono_font = pygame.font.Font(file_resource(
-                         'Roboto_Mono', 'static', 'RobotoMono-Regular.ttf'), 16)
-        self.banner = self.render_text('Hello, World!', Color('white'))
-        self.screen = pygame.display.set_mode((1920, 1080), FULLSCREEN | SCALED)
+                         'Roboto_Mono', 'static', 'RobotoMono-Regular.ttf'), self.font_size)
+        self.screen = pygame.display.set_mode((screen_x, screen_y), FULLSCREEN | SCALED)
+        self.columns = int((screen_x - (self.border_size * 2)) / self.font_size)
+        self.rows = int((screen_y - (self.border_size * 2)) / self.font_size)
         self.running = True
 
     def run(self):
         clock = pygame.time.Clock()
         fps = 60
-        x = centre(1920, self.banner.get_width())
-        y = centre(1080, self.banner.get_height())
         while self.running:
             self.screen.fill(Color('cornflowerblue'))
-            self.screen.blit(self.banner, (x, y))
+            self.print_at('Hello, World!', (0, 0))
             self.handle_events()            
             clock.tick(fps)
             pygame.display.flip()
@@ -41,6 +39,11 @@ class Monolith:
             elif event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
                     self.running = False
+
+    def print_at(self, text, position):
+        graphical_pos = (self.border_size + (position[0] * self.font_size),
+                         self.border_size + (position[1] * self.font_size))
+        self.screen.blit(self.render_text(text, Color('white')), graphical_pos)
 
     def render_text(self, text, colour):
         return self.mono_font.render(text, True, colour)
